613b1fdda722f2108277f9d5cec519fe38692456
Konstantin L <sw.double@gmail.com>
2022-12-25 18:49:29 +0000

    2022-25

--- 2022/25.ts
+++ 2022/25.ts
@@ -9,69 +9,29 @@ function prepareInput(input: string) {
   return splitLines(input);
 }
 
-const D = {
-  2: 2,
-  1: 1,
-  0: 0,
-  '-': -1,
-  '=': -2,
-};
+const digits = [ '=', '-', '0', '1', '2' ];
 
 function toDec(S: string) {
-  return S.split('')
-    .map((c, i) => D[c] * 5 ** (S.length - i - 1))
-    .reduce((a, b) => a + b);
+  let [p, res] = [1, 0];
+  for (let i = S.length - 1; i >= 0; i--) {
+    res += p * (digits.indexOf(S[i]) - 2);
+    p *= 5;
+  }
+  return res;
 }
 
 function toSNAFU(N: number) {
   const res = [];
   while (N !== 0) {
-    let length = 1;
-    while (1) {
-      const d = Object.keys(D).reverse().find(d => {
-        const max = toDec(d + '2'.repeat(length - 1));
-        const min = toDec(d + '='.repeat(length - 1));
-        return N >= min && N <= max;
-      });
-      if (d) {
-        res.push({ length, d });
-        N -= toDec(d.padEnd(length, '0'));
-        break;
-      }
-      length++;
-    }
-  }
-  const a = new Array(res[0].length).fill('0');
-  for (const { length, d } of res) {
-    a[length - 1] = d;
-  }
-  return a.reverse().join('');
-}
-
-const digits = [ '=', '-', '0', '1', '2' ];
-
-function toSNAFU2(N: number) {
-  const res = [];
-  while (N !== 0) {
-    res.push(digits[(N + 2) % 5]);
-    N = Math.floor((N + 2) / 5);
+    N += 2;
+    res.push(digits[N % 5]);
+    N = Math.floor(N / 5);
   }
   return res.reverse().join('');
 }
 
 function runA(input: Input) {
-  // console.log(toSNAFU2(1));
-  // console.log(toSNAFU2(2));
-  // console.log(toSNAFU2(3));
-  // console.log(toSNAFU2(4));
-  // console.log(toSNAFU2(5));
-  // console.log(toSNAFU2(6));
-  // console.log(toSNAFU2(2022));
-  return toSNAFU2(input.map(toDec).reduce((a, b) => a + b));
-}
-
-function runB(input: Input) {
-  //
+  return toSNAFU(input.map(toDec).reduce((a, b) => a + b));
 }
 
 const ex = `1=-0-2
@@ -88,14 +48,12 @@ const ex = `1=-0-2
 1=
 122`;
 assert.strictEqual('2=-1=0', runA(prepareInput(ex)));
-// assert.strictEqual(0, runB(prepareInput(ex)));
 
 type Input = ReturnType<typeof prepareInput>;
 
 const run = async () => {
   const input = await get(`2022/day/${day}/input`);
   console.log(runA(prepareInput(input)));
-  // console.log(runB(prepareInput(input)));
 };
 
 if (require.main === module) {

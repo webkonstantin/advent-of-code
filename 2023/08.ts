d720f3a98ad368fd88808bb981f1063e359c9c8e
Konstantin L <sw.double@gmail.com>
2023-12-08 21:51:37 +0100

    2023-08

---
+++ 2023/08.ts
@@ -0,0 +1,85 @@
+import * as fs from 'fs';
+import * as assert from 'assert';
+
+function part1(input: string) {
+    const [instructions, nodes] = input.split('\n\n');
+    const map = Object.fromEntries(nodes.split('\n').map(n => {
+        const [key, L, R] = n.match(/\w{3}/g);
+        return [key, { L, R }];
+    }));
+    let curr = 'AAA';
+    let i = 0;
+    while (curr !== 'ZZZ') {
+        curr = map[curr][instructions[i % instructions.length] as 'L' | 'R'];
+        i++;
+    }
+    return i;
+}
+
+function gcd(a: number, b: number) {
+    if (b === 0) return a;
+    return gcd(b, a % b);
+}
+
+function lcm(a: number, b: number) {
+    return (a * b) / gcd(a, b);
+}
+
+function part2(input: string) {
+    const [instructions, nodes] = input.split('\n\n');
+    const map = Object.fromEntries(nodes.split('\n').map(n => {
+        const [key, L, R] = n.match(/\w{3}/g);
+        return [key, { L, R }];
+    }));
+
+    const findCycles = (curr: string, i = 0, pos = 0) => {
+        const visited = new Map<string, number>();
+        const zs = [];
+        while (!visited.has(`${curr},${pos}`)) {
+            visited.set(`${curr},${pos}`, i);
+            curr = map[curr][instructions[pos] as 'L' | 'R'];
+            i++;
+            pos = i % instructions.length;
+            if (curr.endsWith('Z')) {
+                zs.push(i);
+            }
+        }
+        // console.log(`${curr},${pos}`, visited);
+        let start = visited.get(`${curr},${pos}`);
+        let len = i - start;
+        // console.log({ start, len, zs });
+        return { start, len, zs };
+    };
+
+    let nums = [];
+    let curr = Object.keys(map).filter(k => k.endsWith('A'))!;
+    for (const c of curr) {
+        nums.push(findCycles(c).zs[0]);
+    }
+    return nums.reduce(lcm);
+}
+
+const sample = `
+LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)
+`.trim();
+
+assert.equal(part1(sample), 6);
+assert.equal(part2(`LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)`), 6);
+
+const input = fs.readFileSync('input08.txt', 'utf8').trim();
+
+console.log(part1(input));
+console.log(part2(input));

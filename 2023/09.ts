ba7020b1e1ede2f62e924d5dcaed8a116989ff1c
Konstantin L <sw.double@gmail.com>
2023-12-09 14:26:13 +0100

    2023-09 cleanup

--- 2023/09.ts
+++ 2023/09.ts
@@ -1,38 +1,21 @@
 import * as assert from 'assert';
 import { getInput } from './get-input';
-import { sum } from 'lodash-es';
+import { range, sum } from 'lodash-es';
 
 const ints = (s: string) => s.match(/-?\d+/g)!.map(Number);
 
 const getNext = (nums: number[]) => {
     if (nums.every(n => n === 0)) return 0;
-    const diffs = [];
-    for (let i = 0; i < nums.length - 1; i++) {
-        diffs.push(nums[i + 1] - nums[i]);
-    }
-    const nextDiff = getNext(diffs);
-    // console.log({ diffs, nextDiff });
-    const next = nums[nums.length - 1] + nextDiff;
-    // console.log({ nums, next });
-    return next;
+    const diffs = range(nums.length - 1).map(i => nums[i + 1] - nums[i]);
+    return nums[nums.length - 1] + getNext(diffs);
 };
 
 function part1(input: string) {
-    const lines = input.split('\n');
-    return sum(lines.map(ints).map(nums => {
-        let next = getNext(nums);
-        // console.log(nums, next);
-        return next;
-    }));
+    return sum(input.split('\n').map(ints).map(getNext));
 }
 
 function part2(input: string) {
-    const lines = input.split('\n');
-    return sum(lines.map(ints).map(nums => {
-        let next = getNext(nums.reverse());
-        // console.log(nums.reverse(), next);
-        return next;
-    }));
+    return sum(input.split('\n').map(ints).map(nums => getNext(nums.reverse())));
 }
 
 const input = await getInput(9);

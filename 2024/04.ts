1374e5c9ff756d69eedef3d45cd44b22553a82b7
Konstantin L <sw.double+github@gmail.com>
2024-12-04 11:57:30 +0100

    2024-04-part2

--- 2024/04.ts
+++ 2024/04.ts
@@ -11,24 +11,15 @@ const part1 = (input: string) => {
 
   let sum = 0;
 
-  const visited = Array.from({ length: H }, () => Array.from({ length: W }, () => false));
-
-  const dfs = (x: number, y: number, dx: number, dy: number, i: number) => {
-    const c = G[y][x];
-    if (c !== WORD[i]) return;
+  const checkWord = (x: number, y: number, dx: number, dy: number, i: number) => {
+    if (G[y][x] !== WORD[i]) return;
     if (i === WORD.length - 1) {
-      // visited[y][x] = true;
-      // console.log(visited.map((row, y) => row.map((v, x) => v ? G[y][x] : '.').join('')).join('\n')+ '\n');
-      // visited[y][x] = false;
       sum++;
       return;
     }
     const [nx, ny] = [x + dx, y + dy];
     if (nx < 0 || nx >= W || ny < 0 || ny >= H) return;
-    if (visited[ny][nx]) return;
-    visited[y][x] = true;
-    dfs(nx, ny, dx, dy, i + 1);
-    visited[y][x] = false;
+    checkWord(nx, ny, dx, dy, i + 1);
   }
 
   for (let y = 0; y < H; y++) {
@@ -36,12 +27,30 @@ const part1 = (input: string) => {
       for (const dx of [-1, 0, 1]) {
         for (const dy of [-1, 0, 1]) {
           if (dx === 0 && dy === 0) continue;
-          dfs(x, y, dx, dy, 0);
+          checkWord(x, y, dx, dy, 0);
         }
       }
     }
   }
 
+  return sum;
+};
+
+const part2 = (input: string) => {
+  const G = input.split('\n');
+  const [W, H] = [G[0].length, G.length];
+  let sum = 0;
+
+  for (let y = 0; y < H - 2; y++) {
+    for (let x = 0; x < W - 2; x++) {
+      const mas1 = G[y][x] + G[y + 1][x + 1] + G[y + 2][x + 2];
+      const mas2 = G[y][x + 2] + G[y + 1][x + 1] + G[y + 2][x];
+      if (
+        [mas1, mas1.split('').reverse().join('')].includes('MAS') &&
+        [mas2, mas2.split('').reverse().join('')].includes('MAS')
+      ) sum++;
+    }
+  }
 
   return sum;
 };
@@ -60,3 +69,18 @@ MXMXAXMASX
 `.trim()), 18);
 
 console.log(part1(input));
+
+assert.equal(part2(`
+MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX
+`.trim()), 9);
+
+console.log(part2(input));
